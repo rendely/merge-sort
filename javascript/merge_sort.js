bc114036aeb604872c3125ce687d642fc21406b5
matthew rendely <matthew.rendely@gmail.com>
2023-07-24 12:46:40 -0700

    solved

--- javascript/merge_sort.js
+++ javascript/merge_sort.js
@@ -1,13 +1,45 @@
 function merge(arr1, arr2) {
-  // type your code here
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < arr1.length || j < arr2.length){
+    if (arr1[i] > arr2[j] || i == arr1.length){
+      result.push(arr2[j]);
+      j++
+    }else{
+      result.push(arr1[i]);
+      i++
+    }
+  }
+  return result;
 }
 
 function mergeSort(arr) {
-  // type your code here
+  if (arr.length <= 1) return arr 
+  let idx = Math.floor(arr.length / 2);
+  let a = arr.slice(0,idx);
+  let b = arr.slice(idx);
+  return merge(mergeSort(a),mergeSort(b))
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: [1, 2, 3, 4]");
+  console.log("=>", merge([1,2],[3,4]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2]");
+  console.log("=>", merge([2], [1]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 1, 1, 5]");
+  console.log("=>", merge([1,5],[1,1]));
+
+  console.log("");
+  console.log("");
+
   console.log("Expecting: [1, 2]");
   console.log("=>", mergeSort([2, 1]));
 
